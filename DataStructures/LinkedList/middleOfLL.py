6a60c883e652b02896fa5f7f0c1eada8760217a2
Prasanth Kumar <dommetiprasanth5821@gmail.com>
2024-03-23 15:34:56 +0530

    middle of the linked list

---
+++ DataStructures/LinkedList/middleOfLL.py
@@ -0,0 +1,10 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow_pointer = head
+        fast_pointer = head
+
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+        # It will return upto the elements from the begining of the slow_pointer starting
+        return slow_pointer
