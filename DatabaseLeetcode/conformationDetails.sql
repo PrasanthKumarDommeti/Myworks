4fdcb43086f785b3b8aad175c97d48716b58a50c
Prasanth Kumar <dommetiprasanth5821@gmail.com>
2024-05-13 22:21:41 +0530

    Simple if else starting program in sql

---
+++ DatabaseLeetcode/conformationDetails.sql
@@ -0,0 +1,2 @@
+--Simple if else statements that has been occured in this scenarios
+select s.user_id, round(avg(if(c.action like 'confirmed',1,0)),2) 'confirmation_rate' from signups s left join confirmations c on s.user_id = c.user_id group by user_id;
