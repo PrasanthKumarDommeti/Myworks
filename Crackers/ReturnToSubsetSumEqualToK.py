1abcb31df827ccf2b413148fc92eb320311eb16e
Prasanth Kumar <dommetiprasanth5821@gmail.com>
2024-07-16 20:47:16 +0530

    difficult problem

---
+++ Crackers/ReturnToSubsetSumEqualToK.py
@@ -0,0 +1,34 @@
+''' 
+    Time Complexity : O(N * (2 ^ N))
+    Space Complexity :  O(N * (2 ^ N))
+
+    Where 'N' denotes the number of elements in the array.
+'''
+
+from copy import deepcopy
+
+def findSubsetsThatSumToKHelper(arr, ans, curSubset, k, idx):
+
+    if(idx == len(arr)) :
+        if(k == 0 and len(curSubset) != 0) :
+            ans.append(curSubset)
+        return
+   
+    # Case when we do not include the current element in the subset.
+    curSubsetCopy = deepcopy(curSubset)
+    findSubsetsThatSumToKHelper(arr, ans, curSubsetCopy, k, idx + 1)
+
+    # Case when we include the current element in the subset.
+    curSubset.append(arr[idx])
+    findSubsetsThatSumToKHelper(arr, ans, curSubset, k - arr[idx], idx + 1)
+
+def findSubsetsThatSumToK(arr, n, k) :
+
+    # List to store the subsets that sum to k.
+    ans = []
+
+    # List to store the current subset.
+    curSubset = []
+    findSubsetsThatSumToKHelper(arr, ans, curSubset, k, 0)
+    
+    return ans
